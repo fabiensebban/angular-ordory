fef14d6fadd75588edcf8a6a5f00c056aaf105c7
Uness Mou <youness.moumou.com>
2017-06-22 16:04:25 +0200

    update models

--- src/app/model/address.ts
+++ src/app/model/address.ts
@@ -1,7 +1,9 @@
 export class Address {
   country: string;
   city: string;
-  zipcode: string;
+  zipcode: number;
   street: string;
   number: number;
+  longitud: string;
+  latitud: string;
 }
--- src/app/model/shop.ts
+++ src/app/model/shop.ts
@@ -1,8 +1,10 @@
 import { Address } from '../model/address';
+import { Type } from '../model/type';
 import { Product } from '../model/product';
 
 export class Shop {
+  id: number;
   name: string;
   address: Address;
-  products: Product[];
+  type: Type;
 }
