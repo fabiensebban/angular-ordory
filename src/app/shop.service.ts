ba4a0b0543166b69f6376d8131075fa43828b5a2
Uness Mou <youness.moumou.com>
2017-06-22 16:45:02 +0200

    create shop service

---
+++ src/app/shop.service.ts
@@ -0,0 +1,12 @@
+import { Injectable } from '@angular/core';
+import { Headers, Http, Response } from '@angular/http';
+import { Shop } from './model/shop';
+
+import 'rxjs/add/operator/toPromise';
+
+@Injectable()
+export class ShopService {
+
+  constructor() { }
+
+}
