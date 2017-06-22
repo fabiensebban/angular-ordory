283a418c0e1045c5806a99fbe394bc8c6b8c1208
ghansum <ghansumn@gmail.com>
2017-06-22 16:18:46 +0200

    récupération des catégories et produits du shop

--- src/app/product-list-item/product-list-item.service.ts
+++ src/app/product-list-item/product-list-item.service.ts
@@ -1,5 +1,6 @@
 import { Injectable } from '@angular/core';
 import { Product } from '../model/product';
+import { Category } from '../model/category';
 
 import 'rxjs/add/operator/toPromise';
 
@@ -9,12 +10,14 @@ export class ProductListItemService {
   constructor() { }
 
   products: Product[];
+  categories: Category[];
 
   getProducts(categories: any): void{
-    for(let category of categories){
+    this.categories = categories;
+    /*for(let category of categories){
     	console.log("category : ",category.name);
-    };
-    console.log(categories[0].name);
+    };*/
+    console.log("categories name : ",this.categories[0].name);
   }
 
 }
