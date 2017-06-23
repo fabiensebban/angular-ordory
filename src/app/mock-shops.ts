dfc7f60d08f87ce8f1495f68bf3c5a1209098da5
Uness Mou <youness.moumou.com>
2017-06-23 03:07:00 +0200

    update mocks

--- src/app/mock-shops.ts
+++ src/app/mock-shops.ts
@@ -4,33 +4,37 @@ export const SHOPS: Shop[] = [
     {
       "id": 733787468,
       "name": "Sushi shop",
+      "longitude": 2.374079,
+      "latitude": 48.850066,
       "address": {
         "country": "France",
         "city": "Paris",
         "zipcode": 75012,
         "street": "Rue de charenton",
         "number": 71,
-        "longitud": "2.374079",
-        "latitud": "48.850066"
+        "longitud": 2.374079,
+        "latitud": 48.850066
       },
       "type": {
         "name": "Chinois"
       }
     },
-    {
-      "id": 364378377,
-      "name": "Pizza Pino",
-      "address": {
-        "country": "France",
-        "city": "Palaiseau",
-        "zipcode": 91120,
-        "street": "Rue Maximilien Robespierre",
-        "number": 21,
-        "longitud": "2.232206",
-        "latitud": "48.718957"
-      },
-      "type": {
-        "name": "Italien"
-      }
-    }
+    // {
+    //   "id": 364378377,
+    //   "name": "Pizza Pino",
+    //   "longitude": 2.232206,
+    //   "latitude": 48.718957,
+    //   "address": {
+    //     "country": "France",
+    //     "city": "Palaiseau",
+    //     "zipcode": 91120,
+    //     "street": "Rue Maximilien Robespierre",
+    //     "number": 21,
+    //     "longitud": 2.232206,
+    //     "latitud": 48.718957
+    //   },
+    //   "type": {
+    //     "name": "Italien"
+    //   }
+    // }
 ];
