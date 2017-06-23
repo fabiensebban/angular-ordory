c618d4da227ac7b7c151d2152341f8877bd9623a
Uness Mou <youness.moumou.com>
2017-06-23 03:08:17 +0200

    create shop service & get data from API

--- src/app/shop.service.ts
+++ src/app/shop.service.ts
@@ -7,6 +7,31 @@ import 'rxjs/add/operator/toPromise';
 @Injectable()
 export class ShopService {
 
-  constructor() { }
+  baseUrl = 'http://api.ordory.ml/shops';  //API url
 
+  //headers params
+  headers = new Headers({
+    'Content-Type': 'application/json; charset=utf-8',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'GET, POST, PUT, DELETE, OPTIONS'
+  });
+
+  constructor(private http: Http) {}
+
+  getShopsByAddress(address): Promise<Shop[]> {
+    let fullUrl = this.baseUrl.concat('?radius=5&address=').concat(address);//radius == 5km TODO implement radius
+    return this.http.get(fullUrl, {headers:this.headers})
+                    .toPromise()
+                    .then(this.setData)
+                    .catch(this.handleError);
+  }
+
+  private setData(shops): Promise<Shop[]> {
+    return Promise.resolve(JSON.parse(shops._body).data);
+  }
+
+  private handleError(error: any): Promise<any> {
+    console.error('ERREUR SHOPS : ', error); // for demo purposes only
+    return Promise.reject(error.message || error);
+  }
 }
