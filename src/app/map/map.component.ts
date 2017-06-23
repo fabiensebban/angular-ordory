b9b1e8efc81a102efa01f5488484f01578e287f5
Uness Mou <youness.moumou.com>
2017-06-23 03:06:25 +0200

    show markers & windowsinfo

--- src/app/map/map.component.ts
+++ src/app/map/map.component.ts
@@ -1,18 +1,27 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnChanges } from '@angular/core';
+import { ShopService } from '../shop.service';
+import { Shop } from '../model/shop';
 
 @Component({
   selector: 'app-map',
   templateUrl: './map.component.html',
-  styleUrls: ['./map.component.css']
+  styleUrls: ['./map.component.css'],
+  providers: [ShopService]
 })
+
 export class MapComponent implements OnInit {
-  lat: number = 48.866667;
+  lat: number = 48.866667;//lat & lng of paris (center)
   lng: number = 2.333333;
-  zoom: number = 11;
+  zoom: number = 10;//zoom of the map
 
-  constructor() { }
+  constructor(private shopService: ShopService) { }
 
-  ngOnInit() {
+  list: Shop[];
+
+  ngOnInit(): void { }
+
+  searchShops(searchInput: string): void {
+    if(searchInput.length > 2)
+      this.shopService.getShopsByAddress(searchInput).then(shops => {this.list = shops});
   }
-
 }
